2814fac56bcd93b772db93319555ca81907d62dc
mestock <MelissaStock44@gmail.com>
2019-05-14 18:23:12 -0700

    Completed all methods with classes

--- list/list-class.js
+++ list/list-class.js
@@ -22,10 +22,39 @@ class List {
    * @returns {*}
    */
   pop() {
-    let returnValue = this.data[this.length];
+    let returnValue = this.data[this.length - 1];
     delete this.data[this.length];
     this.length--;
     return returnValue;
+  }
+
+  shift() {
+    let returnValue = this.data[0];
+    delete this.data[0];
+    this.length--;
+    return returnValue;
+  }
+
+  unshift(list) {
+    let totalidx = this.length + list.length - 1;
+    let reverseidx= this.length - 1;
+    for(totalidx; totalidx >= list.length; totalidx--){
+      this.data[totalidx] = this.data[reverseidx];
+      reverseidx--;
+    }
+    let newidx = list.length - 1;
+    for(newidx; newidx >= 0; newidx--){
+      this.data[newidx] = list[newidx];
+    }
+  
+    this.length = this.length + list.length;
+    return this.length;
+  }
+
+  forEach(callback){
+    for(let i = 0; i < this.length; i++){
+      this.data[i] = callback(this.data[i]);
+    }
   }
 
 }
